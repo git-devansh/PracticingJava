35f1625d07a10c7466a6f32cd66bf482a4b19d45
Devansh <72165346+git-devansh@users.noreply.github.com>
2021-08-27 14:17:24 +0530

    First Commit

---
+++ Codes/P001.java
@@ -0,0 +1,22 @@
+// Data Types and Variables
+
+public class P001 {
+
+	public static void main(String[] args) {
+		
+		int studentAge = 18;
+		double studentGpa = 4.55;
+		String studentFirstName = "Jon";
+		String studentSecondName = "Chena";
+		char lastNameFirstletter = studentSecondName.charAt(0);
+		boolean hasPerfectAttendence = true;
+
+		System.out.println("Student name is " + studentFirstName);
+		System.out.println("Age is " + studentAge);
+		System.out.println("GPA is " + studentGpa);
+		System.out.println("Last Name initial: " + lastNameFirstletter);
+		System.out.println("has perfect attendence?: " + hasPerfectAttendence);
+
+	}
+
+}
