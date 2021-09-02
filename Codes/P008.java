38d97d650bead08a37a54055d5a2817ded2511b7
Devansh <72165346+git-devansh@users.noreply.github.com>
2021-09-02 23:43:53 +0530

    Java Output Formatting

---
+++ Codes/P008.java
@@ -0,0 +1,19 @@
+// Format specifier
+
+public class P008 {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+            	System.out.println("================================");
+            	for(int i=0;i<3;i++){
+                	String s1=sc.next();
+                	int x=sc.nextInt();
+                	//Complete this line
+
+
+
+            	}
+		System.out.printf(s1 + "%d", sc);
+            System.out.println("================================");
+
+	}
+}
