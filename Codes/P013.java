437bae664445d554293e4a181d106ea3b372db89
Devansh <72165346+git-devansh@users.noreply.github.com>
2021-09-07 01:42:43 +0530

    Java End-of-file

---
+++ Codes/P013.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class P013 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        
+        for(int i =1; scanner.hasNext(); i++){
+            System.out.println(i+" "+scanner.nextLine());
+        }
+    }
+}
